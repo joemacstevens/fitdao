21865fbffe810dde88142b9185ffed54e260128b
Joe Stevens <joemac79@gmail.com>
2022-04-10 15:02:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {useAddress, useMetamask} from '@thirdweb-dev/react';
+
+const App = () => {
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Address:", address);
+
+  if(!address){
+      return (
+    <div className="landing">
+      <h1>FitDao</h1>
+      <button onClick={connectWithMetamask} className="btn-hero">Connect your wallet</button>
+    </div>
+  );
+  }
+
+  return (
+        <div className="landing">
+      <h1>👀 wallet connected, now what!</h1>
+    </div>
+  )
+};
+
+export default App;
